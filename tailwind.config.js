057b5111c39740dd8fb9b7fdb26e6f992f6328be
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-10-27 00:38:58 +0800

    Updating tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,28 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "primary-main": "#232967",
+      "primary-second": "#191D4D",
+      "primary-active": "#4EC9D0",
+      "primary-hover": "#353FA4",
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        "primary-gradient-to-t": `linear-gradient(to top, ${theme(
+          "colors.primary-second"
+        )}, ${theme("colors.primary-hover")})`,
+        "primary-gradient-to-b": `linear-gradient(to bottom, ${theme(
+          "colors.primary-second"
+        )}, ${theme("colors.primary-hover")})`,
+        "primary-gradient-to-l": `linear-gradient(to left, ${theme(
+          "colors.primary-second"
+        )}, ${theme("colors.primary-hover")})`,
+        "primary-gradient-to-r": `linear-gradient(to right, ${theme(
+          "colors.primary-second"
+        )}, ${theme("colors.primary-hover")})`,
+      }),
+    },
   },
   plugins: [],
 };
