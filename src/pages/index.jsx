fed468e59a1d06d1f11ed4022db4e496ef80b3da
ersankarimi <ersankarimii311@gmail.com>
2022-10-26 23:15:21 +0800

    refactor: refactor the initial boilerplate
    Refactor the initial boilerplate.
    
    This boilerplate already includes all configurations for this project

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Ouroboros Technologies | Home</title>
+      </Head>
+
+      <h1 className="text-4xl font-bold">
+        Welcome to Ouroboros Technologies Website
+      </h1>
+    </div>
+  );
+}
