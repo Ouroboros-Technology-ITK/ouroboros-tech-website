ee79a5c6a8a5d51272a4575f3059125cb7fe8120
ersankarimi <ersankarimii311@gmail.com>
2022-10-27 16:20:30 +0800

    feat: add classes style

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,7 +5,9 @@ export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main className="flex-grow">{children}</main>
+      <main className="z-10 mb-16  flex-grow px-10 pt-20 md:px-20 xl:px-[120px]">
+        {children}
+      </main>
       <Footer />
     </>
   );
