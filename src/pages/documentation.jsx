504ec2b66b551a03d8b25a70598c865aa9b38b33
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-11-02 21:43:26 +0800

    feat: adding indev pages

---
+++ src/pages/documentation.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+import { InDevelopment } from "../components";
+
+export default function Documentation() {
+  return (
+    <>
+      <Head>
+        <title>Ouroboros Tech | Documentation</title>
+      </Head>
+      <InDevelopment />;
+    </>
+  );
+}
