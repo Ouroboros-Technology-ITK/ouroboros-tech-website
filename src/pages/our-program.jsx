aa9ed99b2f0b554b6695dd6ab39ad690b0e491a1
Dhaffa Agus <73971391+not-xygen@users.noreply.github.com>
2022-11-02 21:43:26 +0800

    feat: adding indev pages

---
+++ src/pages/our-program.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+import { InDevelopment } from "../components";
+
+export default function OurProgram() {
+  return (
+    <>
+      <Head>Ouroboros Tech | Our Program</Head>
+      <InDevelopment />
+    </>
+  );
+}
