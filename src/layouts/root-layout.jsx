7a00b99e556f4f2f045d563445b2633ab592f5ef
Ersan Karimi <ersankarimii311@gmail.com>
2023-03-22 22:32:43 +0800

    chore(classname): update the tailwindcss classnames

--- src/layouts/root-layout.jsx
+++ src/layouts/root-layout.jsx
@@ -37,9 +37,9 @@ const NavLink = forwardRef(({ children, href, ...props }, forwadedRef) => (
     {...props}
     ref={forwadedRef}
     href={href}
-    className="group h-max w-max overflow-hidden rounded-md text-2xl font-medium text-brand-secondary-00/90 outline-none duration-200 hover:text-brand-secondary-00/100 focus:outline-none focus:ring-1 focus:ring-brand-secondary-00/60 active:brightness-90 sm:text-sm md:text-base xl:text-lg"
+    className="group h-max w-max overflow-hidden rounded-md text-2xl font-medium text-brand-secondary-00/90 outline-none duration-200 hover:text-brand-secondary-00/100 focus:text-brand-secondary-00/100 focus:outline-none focus:ring-1 focus:ring-brand-secondary-00/60 active:brightness-90 sm:text-sm md:text-base xl:text-lg"
   >
-    <div className="before:content-[' '] relative z-50 w-max px-1 before:absolute before:bottom-0 before:left-0 before:top-1/2 before:w-5/12 before:rounded-sm before:bg-brand-trinity-06/30 before:transition-all before:duration-200 hover:before:w-full group-hover:before:bg-brand-trinity-06/100 group-focus:before:w-full group-data-[state=on]:before:w-full group-data-[state=on]:before:bg-brand-trinity-06/100">
+    <div className="before:content-[' '] relative z-50 w-max px-1 before:absolute before:bottom-0 before:left-0 before:top-1/2 before:w-5/12 before:rounded-sm before:bg-brand-trinity-06/30 before:transition-all before:duration-200 hover:before:w-full group-hover:before:bg-brand-trinity-06/100 group-focus:before:w-full group-focus:before:bg-brand-trinity-06/100 group-data-[state=on]:before:w-full group-data-[state=on]:before:bg-brand-trinity-06/100">
       <span className="relative">{children}</span>
     </div>
   </Link>
@@ -60,7 +60,7 @@ export const RootLayout = ({ children }) => {
 
   return (
     <>
-      <header className="sticky top-0 flex justify-center bg-brand-secondary-06/90 p-4 backdrop-blur-md">
+      <header className="sticky top-0 z-50 flex justify-center bg-brand-secondary-06/90 p-4 backdrop-blur-md">
         <div className="flex w-full items-center justify-between sm:container">
           <div className="flex items-center gap-3 lg:gap-4 2xl:gap-5">
             <Image
@@ -88,7 +88,7 @@ export const RootLayout = ({ children }) => {
             <ToggleGroupRadix.Root type="single" value={currentPage} asChild>
               <ul
                 className={cn(
-                  "absolute top-full left-0 right-0 bottom-0 h-screen w-full flex-col gap-8 bg-brand-secondary-06 py-8 px-2 pl-8 sm:static sm:flex sm:h-max sm:flex-row sm:gap-3 sm:px-0 sm:py-0 md:gap-4 xl:gap-6 2xl:gap-8",
+                  "absolute top-full left-0 right-0 bottom-0 h-screen w-full flex-col gap-8 bg-brand-secondary-06 py-8 px-2 pl-8 sm:static sm:flex sm:h-max sm:flex-row sm:gap-3 sm:bg-transparent sm:px-0 sm:py-0 md:gap-4 xl:gap-6 2xl:gap-8",
                   {
                     hidden: !menuIsOpen,
                     flex: menuIsOpen,
@@ -103,6 +103,7 @@ export const RootLayout = ({ children }) => {
                     <ToggleGroupRadix.Item
                       value={href}
                       onClick={() => setCurrentPage(href)}
+                      tabIndex={0}
                       asChild
                     >
                       <NavLink href={href}>{label}</NavLink>
@@ -114,8 +115,8 @@ export const RootLayout = ({ children }) => {
           </nav>
         </div>
       </header>
-      <main className="flex-1">{children}</main>
-      <footer className="h-max bg-brand-secondary-06 p-4">
+      <main className="flex-1 px-4 py-20">{children}</main>
+      <footer className="h-max justify-start bg-brand-secondary-06 p-4">
         <div className="container m-auto">Footer</div>
       </footer>
     </>
