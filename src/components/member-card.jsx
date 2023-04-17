c2ec09b4d3343e91e95b7eae5c01473ca8a79ad2
Ersan Karimi <ersankarimii311@gmail.com>
2023-04-18 06:43:20 +0800

    chore: update tailwindcss in member-card component

--- src/components/member-card.jsx
+++ src/components/member-card.jsx
@@ -2,18 +2,15 @@ import Image from "next/image";
 
 export const MemberCard = ({ name, description, imageSrc }) => {
   return (
-    <div className="flex w-full flex-row justify-center gap-6 rounded-lg bg-brand-secondary-07 p-4 transition delay-75 ease-in-out hover:border-brand-secondary-02 hover:drop-shadow-program">
+    <div className="flex w-full cursor-default grid-cols-1 justify-center gap-6 rounded-lg border-[1px] border-brand-secondary-06 bg-brand-secondary-07 p-4 transition delay-75 ease-in-out hover:border-brand-secondary-02 hover:drop-shadow-program">
       <Image
         src={imageSrc}
         alt="Nur Fajri Azhar"
         className="flex h-24 w-24 rounded-full object-cover"
       />
       <div className="flex w-52 flex-col justify-center gap-2">
-        <h4 className="text-18-medium truncate">{name}</h4>
-        <div className="flex flex-row items-center justify-between gap-4">
-          <p className="text-18-regular truncate">{description}</p>
-          <span>{">"}</span>
-        </div>
+        <h4 className="text-lg font-bold">{name}</h4>
+        <p className="text-sm font-extralight">{description}</p>
       </div>
     </div>
   );
