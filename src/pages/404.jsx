f951bfa4b4b96d4468361b4d3365cede32ebbd2c
Ersan Karimi <ersankarimii311@gmail.com>
2023-04-18 06:21:55 +0800

    chore: update tailwindcss classnames

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -15,14 +15,14 @@ const ErrorPage = () => {
         <title>Page Not Found | Ouroboros Technologies</title>
       </Head>
 
-      <section className="container m-auto grid max-w-7xl grid-rows-[max-content_1fr] content-center items-center justify-items-center gap-12 px-4 text-center lg:grid-cols-2 lg:grid-rows-none">
+      <section className="container m-auto grid max-w-7xl grid-rows-[max-content_1fr] content-center items-center justify-items-center gap-12 py-12 px-4 text-center lg:grid-cols-2 lg:grid-rows-none">
         <div className="flex flex-col gap-4">
           <div className="flex flex-col gap-1">
             <h2 className="text-3xl font-semibold lg:text-4xl">Ooops!</h2>
             <h1 className="text-4xl font-bold lg:text-5xl">Page Not Found</h1>
           </div>
           <div className="flex flex-col items-center gap-6">
-            <p className="sm:w-3/5 lg:w-3/4 xl:text-lg">
+            <p className="w-3/5 lg:w-3/4 xl:text-lg">
               Sorry about that! Please visit our homepage to get where you need
               to go.
             </p>
@@ -32,8 +32,11 @@ const ErrorPage = () => {
           </div>
         </div>
 
-        <picture>
-          <Image src={ErrorPageIllustration} />
+        <picture className="flex">
+          <Image
+            src={ErrorPageIllustration}
+            className="h-full w-full object-contain"
+          />
         </picture>
       </section>
     </>
